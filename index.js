0e85249d96e106bcb0850c53cf2e16a0dbc7807d
brokenaadi143A <brokenaadi143@gmail.com>
2024-12-18 22:16:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const { spawn } = require("child_process");
-const { readFileSync } = require("fs-extra");
-const http = require("http");
 const axios = require("axios");
-const semver = require("semver");
 const logger = require("./utils/log");
-
-/////////////////////////////////////////////
-//========= Check node.js version =========//
-/////////////////////////////////////////////
-
-// const nodeVersion = semver.parse(process.version);
-// if (nodeVersion.major < 13) {
-//     logger(`Your Node.js ${process.version} is not supported, it required Node.js 13 to run bot!`, "error");
-//     return process.exit(0);
-// };
 
 ///////////////////////////////////////////////////////////
 //========= Create website for dashboard/uptime =========//
@@ -25,69 +12,66 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// Serve the index.html file
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(port);
-
-logger("Opened server site...", "[ Starting ]");
+// Start the server and add error handling
+app.listen(port, () => {
+    logger(`Server is running on port ${port}...`, "[ Starting ]");
+}).on('error', (err) => {
+    if (err.code === 'EACCES') {
+        logger(`Permission denied. Cannot bind to port ${port}.`, "[ Error ]");
+    } else {
+        logger(`Server error: ${err.message}`, "[ Error ]");
+    }
+});
 
 /////////////////////////////////////////////////////////
 //========= Create start bot and make it loop =========//
 /////////////////////////////////////////////////////////
 
-function startBot(message) {
-    (message) ? logger(message, "[ Starting ]") : "";
+// Initialize global restart counter
+global.countRestart = global.countRestart || 0;
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "PREM-BABU.js"], {
+function startBot(message) {
+    if (message) logger(message, "[ Starting ]");
+
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Priyansh.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
     });
 
     child.on("close", (codeExit) => {
-        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("Restarting...");
+        if (codeExit !== 0 && global.countRestart < 5) {
             global.countRestart += 1;
-            return;
-        } else return;
+            logger(`Bot exited with code ${codeExit}. Restarting... (${global.countRestart}/5)`, "[ Restarting ]");
+            startBot();
+        } else {
+            logger(`Bot stopped after ${global.countRestart} restarts.`, "[ Stopped ]");
+        }
     });
 
-    child.on("error", function (error) {
-        logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
+    child.on("error", (error) => {
+        logger(`An error occurred: ${JSON.stringify(error)}`, "[ Error ]");
     });
 };
+
 ////////////////////////////////////////////////
 //========= Check update from Github =========//
 ////////////////////////////////////////////////
 
+axios.get("https://raw.githubusercontent.com/priyanshu192/bot/main/package.json")
+    .then((res) => {
+        logger(res.data.name, "[ NAME ]");
+        logger(`Version: ${res.data.version}`, "[ VERSION ]");
+        logger(res.data.description, "[ DESCRIPTION ]");
+    })
+    .catch((err) => {
+        logger(`Failed to fetch update info: ${err.message}`, "[ Update Error ]");
+    });
 
-axios.get("https://raw.githubusercontent.com/priyanshu192/bot/main/package.json").then((res) => {
-    logger(res['data']['name'], "[ NAME ]");
-    logger("Version: " + res['data']['version'], "[ VERSION ]");
-    logger(res['data']['description'], "[ DESCRIPTION ]");
-});
+// Start the bot
 startBot();
-/*axios.get("https://raw.githubusercontent.com/d-jukie/miraiv2_fix/main/package.json").then((res) => {
-    const local = JSON.parse(readFileSync('./package.json'));
-    if (semver['lt'](local.version, res['data']['version'])) {
-        if (local.autoUpdate == !![]) {
-            logger('A new update is available, start update processing...', '[ UPDATE ]');
-            const updateBot = {};
-            updateBot.cwd = __dirname
-            updateBot.stdio = 'inherit' 
-            updateBot.shell = !![];
-            const child = spawn('node', ['update.js'], updateBot);
-            child.on('exit', function () {
-                return process.exit(0);
-            })
-            child.on('error', function (error) {
-                logger('Unable to update:' + JSON.stringify(error), '[ CHECK UPDATE ]');
-            });
-        } else logger('A new update is available! Open terminal/cmd and type "node update" to update!', '[ UPDATE ]'), 
-        startBot();
-    } else logger('You are using the latest version!', '[ CHECK UPDATE ]'), startBot();
-}).catch(err => logger("Unable to check update.", "[ CHECK UPDATE ]"));*/
-// THIS BOT WAS MADE BY MR PREM BABU DO NOT CHANGE CREDIT NAME
